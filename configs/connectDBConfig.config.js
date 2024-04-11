43656d6d35bd5424ed380c2c6d609eab177d4ead
TriThuc1506 <125669322+TriThuc1506@users.noreply.github.com>
2024-04-11 19:38:35 +0700

    Update connectDBConfig.config.js

--- configs/connectDBConfig.config.js
+++ configs/connectDBConfig.config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-const mongoDB_URL = `mongodb+srv://${process.env.mongoDB_USER}:${process.env.mongoDB_PASSWORD}@cluster0.js1ml.mongodb.net/${process.env.mongoDB_DB_Name}?retryWrites=true&w=majority`
+const mongoDB_URL = `mongodb+srv://${process.env.mongoDB_USER}:${process.env.mongoDB_PASSWORD}@cluster0.js1ml.mongodb.net/${process.env.mongoDB_DB_Name}?retryWrites=true&w=majority`;
+console.log(mongoDB_URL);
 
 exports.connectDB = async ()=>{
     return await mongoose.connect(mongoDB_URL)
